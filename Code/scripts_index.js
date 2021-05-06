a27cca2d3f7b9c500628ee3b61c2a9992c7d3028
Pavel <Papaskaaaa@gmail.com>
2021-05-07 01:29:41 +0300

    % mob ver

--- Code/scripts_index.js
+++ Code/scripts_index.js
@@ -1,3 +1,38 @@
+//hamburger
+function hamburger(){
+    let hamb = document.querySelector(".open_hamb").style.display    
+    let coord_hamb = 0
+    let coord_closed_hamb = 170
+
+    if(hamb === "" || hamb === "none"){
+        document.querySelector(".open_hamb").style.display = "block"
+        let open_hamb = setInterval(function(){
+            if(coord_hamb < 170){
+                coord_hamb+=5
+                document.querySelector(".open_hamb").style.width = coord_hamb + "px"
+            }
+        },1)
+        setTimeout(()=>clearInterval(open_hamb),150)
+    }else{
+        let closed_hamb = setInterval(function(){
+            if(coord_closed_hamb > 0){
+                coord_closed_hamb-=5
+                document.querySelector(".open_hamb").style.width = coord_closed_hamb + "px"
+            }
+            else{
+                document.querySelector(".open_hamb").style.display = "none"
+            }
+        },1)
+        setTimeout(()=>clearInterval(closed_hamb),150)
+        
+    }
+}
+
+
+
+
+
+
 //slider
 let quantity_slide = document.querySelectorAll(".move_slider > span").length
 let width_slide = document.querySelector(".move_slider > span").offsetWidth
